07604bc44b144506d5ace7c3ad7785e5fec875fe
Jung, DaeCheon <daejung@ea.com>
2019-07-17 13:11:51 +0900

    add ui_effect

---
+++ ui/ui_effect.cpp
@@ -0,0 +1,69 @@
+//
+//  ui_effect.cpp
+//
+//  Created by Jung, DaeCheon on 17/07/2019.
+//
+
+#include "ui_effect.h"
+
+#define STEP 100
+#define STEP_DEGREE 0.15f
+
+void ui_effect::addBG(Node * parent, COLOR_RGB color, bool isRunRotate) {
+    this->setContentSize(parent->getContentSize());
+    float radius = fmax(parent->getContentSize().width, parent->getContentSize().height);
+    
+    mBGLayer = Layout::create();
+    mBGLayer->setContentSize(Size(radius, radius));
+    
+    Color4F c1 = color.getColor4F();
+    Color4F c2 = color.getColorLight().getColor4F();
+    int cnt = 0;
+    const float interval = STEP_DEGREE * 100;
+    for(int n=0; n < 360 / interval; n++) {
+        drawArc(mBGLayer, radius, (n*interval), (n + 1) * interval, (cnt % 2) == 0 ? c1 : c2);
+        cnt++;
+    }
+    
+    Color4B color1 = Color4B(0, 0, 0, 0);
+    mBGGradientLayer = LayerRadialGradient::create( color1
+                                                    , Color4B::BLACK
+                                                    , radius
+                                                    , gui::inst()->getCenter(parent)
+                                                    , 0.3f);
+    
+    mBGGradientLayer->setContentSize(parent->getContentSize());
+    gui::inst()->setAnchorPoint(mBGLayer, ALIGNMENT_CENTER);
+    mBGLayer->setPosition(gui::inst()->getCenter(parent));
+    this->addChild(mBGLayer);
+    this->addChild(mBGGradientLayer);
+    parent->addChild(this);
+}
+
+void ui_effect::drawArc(Node * layer, float radius, float fromDegree, float toDegree, const Color4F color) {
+    CCLOG("%f ~ %f", fromDegree, toDegree);
+    DrawNode * arc = DrawNode::create();
+    vector<Vec2> vecs;
+    Vec2 center = gui::inst()->getCenter(layer);
+    vecs.push_back(center);
+    for(int n=0; n <= (int)STEP; n++) {
+        Vec2 pos;
+        // cos =   x / mRadius
+        // sin =   y / mRadius
+        pos.x = center.x + std::cosf(CC_DEGREES_TO_RADIANS(fromDegree + (n * STEP_DEGREE))) * radius;
+        pos.y = center.y + std::sinf(CC_DEGREES_TO_RADIANS(fromDegree + (n * STEP_DEGREE))) * radius;
+        
+        vecs.push_back(pos);
+    }
+    //vecs.push_back(mCenter);
+    arc->drawSolidPoly(vecs.data(), (unsigned int)vecs.size(), color);
+    layer->addChild(arc);
+}
+
+void ui_effect::runBGRotate(float duration) {
+    auto seq = Sequence::create(RotateBy::create(duration, 360), RotateTo::create(0.01, 0), NULL);
+    mBGLayer->runAction(RepeatForever::create(seq));
+}
+void ui_effect::stopRotate() {
+    mBGLayer->stopAllActions();
+}
---
+++ ui/ui_effect.h
@@ -0,0 +1,37 @@
+//
+//  ui_effect.h
+//
+//  Created by Jung, DaeCheon on 17/07/2019.
+//
+
+#ifndef ui_effect_h
+#define ui_effect_h
+
+#include "ui.h"
+#include "ui_color.h"
+
+class ui_effect : public Layout
+{
+public:
+    static ui_effect * create() {
+        ui_effect * ret = new (std::nothrow) ui_effect;
+        if (ret)
+        {
+            ret->autorelease();
+        } else {
+            CC_SAFE_DELETE(ret);
+        }
+        
+        return ret;
+    };
+    
+    void addBG(Node * parent, COLOR_RGB color, bool isRunRotate = false);
+    void runBGRotate(float duration = 8.f);
+    void stopRotate();
+    
+private:
+    void drawArc(Node * layer, float radius, float fromDegree, float toDegree, const Color4F color);
+    Layout * mBGLayer;
+    LayerRadialGradient * mBGGradientLayer;
+};
+#endif /* ui_effect_h */
